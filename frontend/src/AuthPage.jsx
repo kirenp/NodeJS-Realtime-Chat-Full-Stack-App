929ff23dcb2bcfd0fc4078eb2d84df4f810125f5
kirenp <kiren.0824@gmail.com>
2025-03-19 18:27:50 +0530

    Initial commit - Dockerized React-Vite and Node.js project

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -1,11 +1,14 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001"; // Use the environment variable
+
 const AuthPage = (props) => {
   const onSubmit = (e) => {
     e.preventDefault();
     const { value } = e.target[0];
+
     axios
-      .post("http://localhost:3001/authenticate", { username: value })
+      .post(`${API_URL}/authenticate`, { username: value })
       .then((r) => props.onAuth({ ...r.data, secret: value }))
       .catch((e) => console.log("Auth Error", e));
   };
@@ -14,15 +17,11 @@ const AuthPage = (props) => {
     <div className="background">
       <form onSubmit={onSubmit} className="form-card">
         <div className="form-title">Welcome 👋</div>
-
         <div className="form-subtitle">Set a username to get started</div>
-
         <div className="auth">
           <div className="auth-label">Username</div>
           <input className="auth-input" name="username" />
-          <button className="auth-button" type="submit">
-            Enter
-          </button>
+          <button className="auth-button" type="submit">Enter</button>
         </div>
       </form>
     </div>
